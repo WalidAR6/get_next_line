2a2e0a0a0e0ec849e1c2cd29bbde13e9a5686515
Walid Araissi <waraissi@e1r7p6.1337.ma>
2022-11-18 11:27:03 +0100

    functions help us to reach the bonus objectif

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 11:26:01 by waraissi          #+#    #+#             */
+/*   Updated: 2022/11/18 11:26:26 by waraissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (dst == NULL || n == 0)
+		return (ft_strlen(src));
+	while (src[i] && i < n - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*p;
+
+	p = malloc((ft_strlen(str) + 1) * sizeof(char));
+	if (!p)
+		return (NULL);
+	ft_strlcpy(p, str, ft_strlen(str) + 1);
+	return (p);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*p;
+
+	i = 0;
+	j = 0;
+	if (!s2)
+		return (NULL);
+	p = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!p)
+		return (NULL);
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		p[i + j] = s2[j];
+		j++;
+	}
+	p[i + j] = 0;
+	return (free(s1), s1 = NULL, p);
+}
