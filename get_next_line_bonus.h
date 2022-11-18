b0fac56a0fbb461c5344dfc30a024c86e8588f85
Walid Araissi <waraissi@e1r7p6.1337.ma>
2022-11-18 11:33:06 +0100

    header file bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 11:27:53 by waraissi          #+#    #+#             */
-/*   Updated: 2022/11/18 11:30:16 by waraissi         ###   ########.fr       */
+/*   Updated: 2022/11/18 11:32:36 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
