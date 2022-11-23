90c48b32427b83f404c068207ba1ce10ca98062e
Walid Araissi <waraissi@e1r2p9.1337.ma>
2022-11-23 17:09:48 +0100

    header file bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 11:27:53 by waraissi          #+#    #+#             */
-/*   Updated: 2022/11/18 15:18:45 by waraissi         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:09:21 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include<unistd.h>
 # include<stdlib.h>
 # include<limits.h>
+# include<fcntl.h>
 
 size_t	ft_strlen(char *str);
 size_t	ft_strlcpy(char *dst, char *src, size_t n);
